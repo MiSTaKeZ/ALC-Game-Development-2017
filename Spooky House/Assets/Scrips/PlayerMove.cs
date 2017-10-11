111453b7966068795cdab3a9650a90b469184065
Unknown <ytmistakez@gmail.com>
2017-10-11 13:07:30 -0600

    PlayerMove script!

--- Spooky House/Assets/Scrips/PlayerMove.cs
+++ Spooky House/Assets/Scrips/PlayerMove.cs
@@ -5,15 +5,15 @@ public class PlayerMove : MonoBehaviour {
 	public float moveSpeed;
 	public float turnSpeed;
 	public float jumpHeight;
-	
+
 
 
 	void Update () {
 		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
-		var y = Input.GetAxis("Horizontal")* Time.deltaTime *turnSpeed;
-		var z = Input.Getaxis("Vertical")* Time.deltaTime * moveSpeed;
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
 
-		transfrom.Rotate(0,y,0);
+		transform.Rotate(0,y,0);
 		transform.Translate(0,0,z);
 		transform.Translate(0,j,0);
 	}
