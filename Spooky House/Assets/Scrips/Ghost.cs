493905f3d14dd58e0edf0b16bc190426691a7270
Unknown <ytmistakez@gmail.com>
2017-10-13 13:14:52 -0600

    Ghost

---
+++ Spooky House/Assets/Scrips/Ghost.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghost : MonoBehaviour {
+
+	public float moveSpeed;
+	public Transform target;
+	public int damage;
+
+	
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player"){ 
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+	}
+	
+
+
+	// void OnCollisionEnter(Collision other)
+	// {
+	// 	var hit = other.gameObject;
+	// 	var health = hit.GetComponet<playerHealth>();
+	// }
+		
+	
+}
