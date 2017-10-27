abc19d8f95e49e74b540e4d05179f00281a52fea
Unknown <ytmistakez@gmail.com>
2017-10-27 13:11:52 -0600

    New colors and on and of flashlight!!!

--- Spooky House/Assets/Scrips/FlashLight.cs
+++ Spooky House/Assets/Scrips/FlashLight.cs
@@ -1,35 +1,33 @@
-﻿using Styem.Collections;
-using UnityEngine;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class FlashLight; MonoBehaviour {
-	public bool light on;
+public class FlashLight : MonoBehaviour {
+	
+	public bool lightOn;
 
 	Light light;
 
-
-
 	// Use this for initialization
 	void Start () {
-		Light = GetComponent<Light> ();
+		light = GetComponent<Light> ();
 		//Set Light default to ON
 		lightOn = true; 
-		light.enable = true;
-
+		light.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	// Togle light on/off when L key is pressed.
-	if (Input.GetKeyUp (KeyCode.F) && lightOn)  {
-		lightOn = false;
-		light.enable = false;
+		// Togle light on/off when F key is pressed.
+		if (Input.GetKeyUp (KeyCode.F) && lightOn)  {
+			lightOn = false;
+			light.enabled = false;
 
-	}
+		}
 
-	else if (Input.GetKeyUp (KeyCode.F) && !lightOn){
-		lightOn = true;
-		light.enabled = true;
+		else if (Input.GetKeyUp (KeyCode.F) && !lightOn){
+			lightOn = true;
+			light.enabled = true;
 
 		}
 	}
