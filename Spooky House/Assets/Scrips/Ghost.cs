921db5b5d546389f3cd36020a33334461b528fb8
Unknown <ytmistakez@gmail.com>
2017-11-10 13:13:05 -0700

    Ghost

--- Spooky House/Assets/Scrips/Ghost.cs
+++ Spooky House/Assets/Scrips/Ghost.cs
@@ -8,14 +8,39 @@ public class Ghost : MonoBehaviour {
 	public int damage;
 
 	
-	void OnTriggerStay(Collider other)
-	{
-		if(other.gameObject.name == "Player"){ 
-			transform.LookAt(target);
-			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.name == "Player"){
+			Follow();
+		}
+		else{
+			print("Ghost is grounded");
+			Wander();
 		}
 	}
 	
+	void Wander(){
+		transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		int randomNum = Random.Range(0,360);
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+
+
+		Debug.DrawRay(transform.position,fwd*3,Color.red);
+
+		if(Physics.Raycast(transform.position,fwd,out hit,3)){
+
+			if(hit.collider.tag == "Wall"){
+				transform.Rotate(0,randomNum,0);
+			}
+		}
+	}
+	
+	void Follow(){
+		transform.LookAt(target); 
+		transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+	}
+
+
 
 
 	// void OnCollisionEnter(Collision other)
@@ -24,5 +49,4 @@ public class Ghost : MonoBehaviour {
 	// 	var health = hit.GetComponet<playerHealth>();
 	// }
 		
-	
-}
+}	
