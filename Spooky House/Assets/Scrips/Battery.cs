490e0adb51a853e759c1c832d073ac67f1d1a7bd
Unknown <ytmistakez@gmail.com>
2017-12-15 13:11:48 -0700

    blasaa

--- Spooky House/Assets/Scrips/Battery.cs
+++ Spooky House/Assets/Scrips/Battery.cs
@@ -32,5 +32,5 @@ public class Battery : MonoBehaviour {
 			Destroy(gameObject);
 		}
 	}
-
+	
 }
---
+++ Spooky House/Assets/Scrips/BatterySpawn.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatterySpawn : MonoBehaviour {
+
+	public GameObject battery;
+
+	pulbic Transform spawnPoint;
+
+	public float spawnTime;
+
+	public bool batSpawned;
+
+
+
+	// Use this for initialization
+	void Start () {
+		batSpawned = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(batSpawned == False){
+			StartCoroutine(SpawnBat, battery));
+			
+		}
+		else{
+			StopCoroutine(SpawnBat(spawnTime, battery));
+		}
+	}
+
+	void OnTriggerEnter(Collisoin other){
+		if(other.GameObject.tag == "battery"){
+			batSpawned == True;
+		}
+		else{
+			batSpawned == False;
+		}
+	
+	}
+
+	Void IEnumerator SpawnBat(floast time, Rigidbody bat){
+		yield return new WaitForSeconds(time);
+			Instantiate(battery, spawnPoint.position, spawnPoint.rotation);
+	
+		
+
+
+	} 
+}
